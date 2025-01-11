47a76b119574c113e1000a6904307ed9215366d6
ashuOO7 <ashjnavj@gmail.com>
2025-01-12 03:22:32 +0530

    render list and controller logic

---
+++ app/controllers/api/places_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  class PlacesController < ApplicationController
+  def index
+    places = Place.all.map do |place|
+      {
+        name: place.name,
+        city: place.city,
+        latest_download_speed: latest_download_speed(place),
+        latest_download_units: latest_download_units(place),
+        number_of_measurements: number_of_measurements(place)
+      }
+    end
+
+    render(json: { places: places })
+  end
+
+  def latest_download_speed(place)
+    place.internet_speeds.order("created_at DESC").last.download_speed
+  end
+
+  def latest_download_units(place)
+    place.internet_speeds.order("created_at DESC").last.download_units
+  end
+
+  def number_of_measurements(place)
+    place.internet_speeds.count
+  end
+  
+  end
+end
