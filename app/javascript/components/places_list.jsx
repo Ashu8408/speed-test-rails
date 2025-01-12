8d757775154d61e1a5693d063f917e1f75ba0818
ashuOO7 <ashjnavj@gmail.com>
2025-01-13 00:33:12 +0530

    correcting ids and errors

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -3,29 +3,39 @@ import ReactDOM from "react-dom/client";
 
 function renderPlacesPage(body) {
   return (
-    <div class="bg-white p-8 rounded-md w-full">
-      <div class="flex items-center justify-between pb-6">
+    <div className="bg-white p-8 rounded-md w-full">
+      <div className="flex items-center justify-between pb-6">
         <div>
-          <h2 class="text-4xl text-gray-600 font-semibold">Places</h2>
+          <h2 className="text-4xl text-gray-600 font-semibold">Places</h2>
         </div>
-        <div class="flex items-center justify-between">
-        <div className="flex bg-gray-50 items-center p-2 rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-              fill="currentColor">
-              <path
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                />
+        <div className="flex items-center justify-between">
+          <div className="flex bg-gray-50 items-center p-2 rounded-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
             </svg>
-            <input class="bg-gray-50 outline-none ml-1 block w" type="text" name="" id="" placeholder="search..." />
+            <input
+              class="bg-gray-50 outline-none ml-1 block w"
+              type="text"
+              name=""
+              id=""
+              placeholder="search..."
+            />
           </div>
-          <div class="lg:ml-40 ml-10 space-x-8">
-            <button class="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">New Log</button>
+          <div className="lg:ml-40 ml-10 space-x-8">
+            <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">
+              New Log
+            </button>
           </div>
         </div>
       </div>
       {body}
     </div>
-  )
+  );
 }
 
 function PlacesList() {
@@ -36,66 +46,71 @@ function PlacesList() {
   const [loadedPlaces, setloadedPlaces] = useState([]);
 
   useEffect(() => {
-    //hit the server to get places list
-    const apiEndpoint = "/api/places";
-
-    fetch(apiEndpoint)
+    console.log("Fetching data...");
+    fetch("/api/places")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setloadedPlaces(data["places"]);
+        console.log("API Response:", data);
+        setloadedPlaces(data.places);
         setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
+  const onSearchTextChange = (e) => {
+    console.log("onSearchTextChange was executed!");
+    setLoading(true);
+    setSearchTerm(e.target.value);
+  };
+
   const loadingSection = <div>Loading page...</div>;
-  const tableHeaderClass="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left"
+  // const tableHeaderClass ="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left";
   const dataSection = loadedPlaces.map((place, index) => (
     <div>
-			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+      <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table className="min-w-full leading-normal">
             <thead>
               <tr>
-                <th className={tableHeaderClass}>Name</th>
-                <th className={tableHeaderClass}>City</th>
-                <th className={tableHeaderClass}>Recent Upload Speed</th>
-                <th className={tableHeaderClass}>Recent Upload Speed Units</th>
-                <th className={tableHeaderClass}>Number of measurements</th> 
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                  Name
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                  City
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                  Recent Upload Speed
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                  Recent Upload Speed Units
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
+                  Number of Measurements
+                </th>
               </tr>
             </thead>
             <tbody>
-              {loadedPlaces.map((place, index) => {
-                return (
-                  <tr key={index}>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div class="flex items-center">
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">{place.name}</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">{place.city}</p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        {place.most_recent_download_speed}
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        {place.most_recent_download_units}
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        {place.number_of_measurements}
-                      </p>
-                    </td>
-                  </tr>
-                )
-              })}
+              {loadedPlaces.map((place, index) => (
+                <tr key={index}>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {place.name}
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {place.city}
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {place.most_recent_download_speed}
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {place.most_recent_download_units}
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {place.number_of_measurements}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -104,14 +119,12 @@ function PlacesList() {
   ));
 
   if (loading) {
-    return renderPlacesPage(loadingSection)
+    return renderPlacesPage(loadingSection);
   } else {
-    return renderPlacesPage(dataSection)
+    return renderPlacesPage(dataSection);
   }
 }
 
 // Render the component into the div with id "places_list_container"
-const placesList = ReactDOM.createRoot(
-  document.getElementById("places_list_container")
-);
+const placesList = ReactDOM.createRoot(document.getElementById("page-places"));
 placesList.render(<PlacesList />);
