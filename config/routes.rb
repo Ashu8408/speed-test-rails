87a0163bcf30107da7858a7dca4884afd30cf950
ashuOO7 <ashjnavj@gmail.com>
2025-01-27 01:35:23 +0530

    react handles routes and added plain form

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "places#index"
+  root "react#home"
+  get "*path", to: "react#home"
 
   get "/api/places", to: "api/places#index"
   
