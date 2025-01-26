e96ec576517b842b3a6652aed1b33307cc80b85a
ashuOO7 <ashjnavj@gmail.com>
2025-01-26 20:37:09 +0530

    fixed double table listing

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-function renderPlacesPage(body) {
+function renderPlacesPage(body, onSearchTextChange) {
   return (
     <div className="bg-white p-8 rounded-md w-full">
       <div className="flex items-center justify-between pb-6">
@@ -10,12 +10,7 @@ function renderPlacesPage(body) {
         </div>
         <div className="flex items-center justify-between">
           <div className="flex bg-gray-50 items-center p-2 rounded-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
               <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
             </svg>
             <input
@@ -24,6 +19,7 @@ function renderPlacesPage(body) {
               name=""
               id=""
               placeholder="search..."
+              onChange={onSearchTextChange}
             />
           </div>
           <div className="lg:ml-40 ml-10 space-x-8">
@@ -44,62 +40,62 @@ function PlacesList() {
 
   const [loading, setLoading] = useState(true);
   const [loadedPlaces, setloadedPlaces] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
 
   useEffect(() => {
     console.log("Fetching data...");
-    fetch("/api/places")
+
+    const apiEndpoint = `/api/places?search_term=${searchTerm}`
+    fetch(apiEndpoint)
       .then((response) => response.json())
       .then((data) => {
-        console.log("API Response:", data);
+        console.log("API Response:--", data);
         setloadedPlaces(data.places);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [searchTerm]);
 
   const onSearchTextChange = (e) => {
-    console.log("onSearchTextChange was executed!");
+    console.log("onSearchTextChange was clicked and executed!");
     setLoading(true);
     setSearchTerm(e.target.value);
   };
 
   const loadingSection = <div>Loading page...</div>;
   // const tableHeaderClass ="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left";
-  const dataSection = loadedPlaces.map((place, index) => (
-    <div>
-      <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+  const tableHeaderClass = "px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+  const dataSection = (
+		<div>
+			<div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+				<div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table className="min-w-full leading-normal">
             <thead>
               <tr>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
-                  Name
-                </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
-                  City
-                </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
-                  Recent Upload Speed
-                </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
-                  Recent Upload Speed Units
-                </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left">
-                  Number of Measurements
-                </th>
+                <th className={tableHeaderClass}>Name</th>
+                <th className={tableHeaderClass}>City</th>
+                <th className={tableHeaderClass}>Recent Upload Speed</th>
+                <th className={tableHeaderClass}>Recent Upload Speed Units</th>
+                <th className={tableHeaderClass}>Number of measurements</th> 
               </tr>
             </thead>
             <tbody>
-              {loadedPlaces.map((place, index) => (
-                <tr key={index}>
-                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    {place.name}
-                  </td>
-                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    {place.city}
-                  </td>
+              {loadedPlaces.map((place, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <div className="flex items-center">
+                        <div className="ml-3">
+                          <p className="text-gray-900 whitespace-no-wrap">{place.name}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">{place.city}</p>
+                    </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     {place.most_recent_download_speed}
                   </td>
@@ -110,13 +106,13 @@ function PlacesList() {
                     {place.number_of_measurements}
                   </td>
                 </tr>
-              ))}
+                )})}
             </tbody>
           </table>
         </div>
       </div>
     </div>
-  ));
+  )
 
   if (loading) {
     return renderPlacesPage(loadingSection);
