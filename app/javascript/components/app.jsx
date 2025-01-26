a71d3ee997a3a612ce59bf1c7bd88372e4c6c131
ashuOO7 <ashjnavj@gmail.com>
2025-01-27 00:19:40 +0530

    setup anchor link to new log button and add routes in react

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter,  Routes,  Route,} from "react-router-dom";
 
 import PlacesList from "./places_list"
+import NewInternetSpeed from "./new_internet_speed"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/new-internet-speed" element={<NewInternetSpeed />} />
         <Route path="*" element={<PlacesList />} />
       </Routes>
     </BrowserRouter>
--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom/client";
+import { Link } from "react-router-dom";
 
 function renderPlacesPage(body, onSearchTextChange) {
   return (
@@ -23,9 +23,9 @@ function renderPlacesPage(body, onSearchTextChange) {
             />
           </div>
           <div className="lg:ml-40 ml-10 space-x-8">
-            <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">
-              New Log
-            </button>
+          <Link to="/new-internet-speed">
+              <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">New Log</button>
+            </Link>
           </div>
         </div>
       </div>
@@ -120,5 +120,3 @@ export default function PlacesList() {
     return renderPlacesPage(dataSection, onSearchTextChange);
   }
 }
-
-// Render the component into the div with id "places_list_container"
